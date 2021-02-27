41b79d2bef9e1a1317d74ab686aea32fbf5f0ec1
Yves Loeys <yves.loeys@gmail.com>
2021-02-27 10:07:01 -0800

    setting up redux

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import Reduxthunk from "redux-thunk";
+import placesReducer from "./store/places-reducer";
+
 import PlacesNavigator from "./navigation/PlacesNavigator";
 
+const rootReducer = combineReducers({
+  places: placesReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(Reduxthunk));
+
 export default function App() {
-  return <PlacesNavigator />;
+  return (
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
+  );
 }
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,11 +1,42 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  ScrollView,
+  Button,
+} from "react-native";
+import Colors from "../constants/Colors";
 
 const NewPlaceScreen = () => {
+  const [titleValue, setTitleValue] = useState("");
+
+  //getting the userinput
+  const titleChangeHandler = (text) => {
+    //add validation later
+    setTitleValue(text);
+  };
+
+  //save the userinput
+  const savePlaceHandler = () => {};
+
   return (
-    <View>
-      <Text>NewPlaceScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>NewPlaceScreen</Text>
+        <TextInput
+          style={styles.textInput}
+          value={titleValue}
+          onChangeText={titleChangeHandler}
+        />
+        <Button
+          title="Save Location"
+          color={Colors.primary}
+          onPress={() => {}}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -26,6 +57,21 @@ NewPlaceScreen.navigationOptions = (navData) => {
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    margin: 30,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+});
 
 export default NewPlaceScreen;
