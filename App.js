eebae86dc145cdcf940ff1491573d9e965320247
Yves Loeys <yves.loeys@gmail.com>
2021-02-28 08:54:50 -0800

    data storage with SQLite

--- App.js
+++ App.js
@@ -3,6 +3,16 @@ import { Provider } from "react-redux";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import Reduxthunk from "redux-thunk";
 import placesReducer from "./store/places-reducer";
+import { init } from "./helpers/db";
+
+init()
+  .then(() => {
+    console.log("Initialized db");
+  })
+  .catch((err) => {
+    console.log("Initialisation failed");
+    console.log(err);
+  });
 
 import PlacesNavigator from "./navigation/PlacesNavigator";
 
